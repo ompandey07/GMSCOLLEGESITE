814fc45793b341460d88dde550da62437ae756d0
Ꭷʍɛ ֆραяк <iamompandey.it@gmail.com>
2025-03-20 20:10:49 +0545

    Update settings.py

--- GMSCOLLEGESITE/settings.py
+++ GMSCOLLEGESITE/settings.py
@@ -78,16 +78,15 @@ WSGI_APPLICATION = 'GMSCOLLEGESITE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'postgres',
-        'PASSWORD': 'admin@1200',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")  # Fetch DB URL from environment variables
+    )
 }
+
 
 
 
